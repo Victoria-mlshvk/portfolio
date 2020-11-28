752fa4da25d8a86c9ded45693da947d76eb04968
Vica <vicafood@gmail.com>
2020-11-28 18:11:17 +0000

    final

---
+++ dist/js/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger'),
+        menu = document.querySelector('.menu'),
+        closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const percents = document.querySelectorAll('.skills__percent_item-number'),
+        lines = document.querySelectorAll('.skills__percent_item-line span');
+
+percents.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
